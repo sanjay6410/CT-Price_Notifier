83a84da16eca8b23933e3e668aceb14e3acf6941
rs guptha <rs.guptha@valtech.com>
2023-06-15 12:02:24 +0530

    intial-backend-project-setup

---
+++ CT-Price_Notifier/src/main/java/com/CTPrice_Notifier/CtPriceNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.CTPrice_Notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtPriceNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtPriceNotifierApplication.class, args);
+	}
+
+}
---
+++ CT-Price_Notifier/src/test/java/com/CTPrice_Notifier/CtPriceNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CTPrice_Notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtPriceNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
